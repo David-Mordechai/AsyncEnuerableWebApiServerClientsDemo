ce821234aeea64ef15043cab9ead9694b589a779
David Mordechai <53663592+David-Mordechai@users.noreply.github.com>
2024-07-10 16:19:47 +0300

    Update App.vue

--- vue-client/src/App.vue
+++ vue-client/src/App.vue
@@ -39,7 +39,7 @@ function formatDate(dateString: string) {
 
 <template>
   <div class="container">
-    <table class="table table-striped table-bordered">
+    <table>
       <thead>
         <tr>
           <th>Date</th>
